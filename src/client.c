505e563336b843ce720ded0cf82a15fcfec0b800
Hithem EL KAMEL <hel-kame@e1r11p4.clusters.42paris.fr>
2022-12-19 16:17:03 +0100

    add(minitalk)

--- src/client.c
+++ src/client.c
@@ -6,22 +6,39 @@
 /*   By: hel-kame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 22:20:15 by hel-kame          #+#    #+#             */
-/*   Updated: 2022/12/17 22:25:12 by hel-kame         ###   ########.fr       */
+/*   Updated: 2022/12/19 16:12:52 by hel-kame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(int argc, char **argv)
+void	one_handler(int signum)
 {
-	union sigval sigval;
+	(void)signum;
+	ft_printf("USR1 : Sending 0 to server (\U0000FB2A).\n");
+}
+
+void	two_handler(int signum)
+{
+	(void)signum;
+	ft_printf("USR2 :Sending 1 to server.\n");
+}
+
+int		main(int argc, char **argv)
+{
 	pid_t pid;
 
 	pid = ft_atoi(argv[1]);
-	if (argc < 3 || pid < 0)
+	if (argc < 2 || pid < 0)
 		return (-1);
-	sigval.sival_int = atoi(argv[2]);
-	ft_printf("sender: sending %d to PID %d\n", sigval.sival_int, pid);
-	sigqueue(pid, SIGUSR1, sigval);
+	ft_printf("Try to sending something to PID %d...\n", pid);
+	sleep(1);
+	signal(SIGUSR1, one_handler);
+	kill(pid, SIGUSR1);
+	kill(getpid(), SIGUSR1);
+	sleep(1);
+	signal(SIGUSR2, two_handler);
+	kill(pid, SIGUSR2);
+	kill(getpid(), SIGUSR2);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: hel-kame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 21:54:08 by hel-kame          #+#    #+#             */
-/*   Updated: 2022/12/17 22:35:46 by hel-kame         ###   ########.fr       */
+/*   Updated: 2022/12/19 15:53:07 by hel-kame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void	signal_handler(int signum, siginfo_t *siginfo, void *ucontext)
 {
+	int i;
+
 	(void)ucontext;
-	if (signum != SIGUSR1)
+	if (signum != SIGUSR1 && signum != SIGUSR2)
 		return ;
-	if (siginfo->si_code != SI_QUEUE)
+	if (siginfo->si_code != SI_USER)
 		return ;
-	ft_printf("%d\n", siginfo->si_int);
+	if (siginfo->si_signo == SIGUSR1)
+		i = 0;
+	if (siginfo->si_signo == SIGUSR2)
+		i = 1;
+	ft_printf("%d\n", i);
 }
 
 int	main(void)
@@ -33,6 +39,7 @@ int	main(void)
 	sigemptyset(&signal_action.sa_mask);
 	signal_action.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &signal_action, NULL);
+	sigaction(SIGUSR2, &signal_action, NULL);
 	while (1)
 		pause();
 	return (0);
